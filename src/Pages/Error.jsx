1f23bda1528119638a5ce96faa187b89f7b1dba3
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-04-14 10:07:43 +0200

    [JSX/CSS] Réalisation et style de la page 404 :boom:

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from 'react'
+import '../Styles/Pages/Error.css'
+import Header from '../Components/Header'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
     return (
-        <div>
-            
+        <div className="Error">
+            <Header />
+            <div className="Error__content">
+                <p className="Error__content__p--404">404</p>
+                <p className="Error__content__p--Oups">
+                    Oups! La page que vous demandez n'éxiste pas.
+                </p>
+                <Link className="Error__content__link" to={'./'}>
+                    Retourner sur la page d'accueil
+                </Link>
+            </div>
         </div>
-    );
-};
+    )
+}
 
-export default Error;
+export default Error
